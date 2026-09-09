fb7d714b21d528c985a415f9fc3f1e9c819c1075
jamal Ferguson <jamal.ferguson1990@gmail.com>
2026-09-08 23:02:12 -0700

    Restore roadside customer exchange scene

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
         <div className="story-photo">
           <img
             src="/images/middle-quarters-roadside-recreation.jpg"
-            alt="Artistic recreation of a Jamaican roadside shrimp vendor with colorful baskets and head-on pepper shrimp served in an open bag"
+            alt="Artistic recreation of a Jamaican roadside vendor handing a small bag of head-on pepper shrimp to a customer"
           />
           <span>Roadside tradition · artistic recreation</span>
         </div>
